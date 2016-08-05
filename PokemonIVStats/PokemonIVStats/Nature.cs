b059c4d655efc7a17556d1b58d496f57d4804cb7
TurtleXYZ <mikkelzuuu@gmail.com>
2016-08-05 19:15:40 +0200

    Revert "added nature list back."
    This reverts commit f3779fbb6a41ddbcbab03d2ee2751a74d2fe18e1.

--- PokemonIVStats/PokemonIVStats/Nature.cs
+++ PokemonIVStats/PokemonIVStats/Nature.cs
@@ -13,24 +13,22 @@ namespace PokemonIVStats
     {
         public static List<Nature> NatureList = new List<Nature>();
         private string name_ = "";
-        private string increasedStat = "";
-        private string decreasedStat = "";
 
         public static void Init()
         {
             try
             {
-                using (StreamReader sr = new StreamReader("Pokemon/naturedata.txt"))
+                using (StreamReader sr = new StreamReader("Pokemon/nature.txt"))
                 {
                     string line = "";
                     while ((line = sr.ReadLine()) != null)
                     {
                         string[] spl = line.Split(';');
-                        if (spl.Length == 3)
+                        if (spl.Length == 9)
                         {
                             try
                             {
-                                new Nature(spl[0], spl[1], spl[2]);
+                                new Pokemon(Convert.ToInt32(spl[0]), spl[1], Convert.ToInt32(spl[2]), Convert.ToInt32(spl[3]), Convert.ToInt32(spl[4]), Convert.ToInt32(spl[5]), Convert.ToInt32(spl[6]), Convert.ToInt32(spl[7]), Convert.ToInt32(spl[8]));
                             }
                             catch (Exception ex)
                             {
@@ -47,13 +45,9 @@ namespace PokemonIVStats
             }
         }
 
-        public Nature(string naturename, string increased, string decreased)
+        public Nature(string naturename)
         {
             name_ = naturename;
-            increased = increasedStat;
-            decreased = decreasedStat;
-
-            NatureList.Add(this);
         }
 
         public string Name
